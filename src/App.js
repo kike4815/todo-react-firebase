11501fdf84bac18fd9e71d67f9644a6b964cac7a
kike4815 <e.pedros.sanchez@gmail.com>
2020-09-02 19:32:55 +0200

    add node-sass, bootstrap and start with components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Container, Col, Row } from 'react-bootstrap'
+
+import './App.scss'
 
 function App () {
-    return <div className="App">TODO react-firebase</div>
+    return (
+        <Container fluid className="app">
+            <div className="title">
+                <h1>ENRIC PEDRÓS SANCHEZ</h1>
+            </div>
+        </Container>
+    )
 }
 
 export default App
